370339eff3b0bf0e21144a77ae894122c19d2269
Eric Lindesay <psyel1@nottingham.ac.uk>
2022-07-14 18:27:41 +0100

    code formatting changes and comments added on some

--- modules/__init__.py
+++
@@ -1,6 +0,0 @@
-#import generateReadme as generateReadme
-#import getURLInfo as getURLInfo
-#import formatProblemStatement as formatProblemStatement
-#import formatExample as formatExample
-#import formatName as formatName
-#import regexConvert as regexConvert
